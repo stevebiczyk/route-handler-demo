898964f0004d5a1ac65d8bead685d80af9c0f458
stevebiczyk <stevebiczyk@gmail.com>
2025-03-18 11:26:23 +0000

    hadling DELETE requests

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -21,3 +21,14 @@ export async function PATCH(
   comments[index].text = text;
   return Response.json(comments[index]);
 }
+
+export async function DELETE(
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComment);
+}
