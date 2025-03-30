1271e0f147580f7b8a506fc82b0175463e7eba22
stevebiczyk <stevebiczyk@gmail.com>
2025-03-30 21:11:29 +0100

    cookies in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   // const requestHeaders = new Headers(request.headers);
@@ -7,9 +7,17 @@ export async function GET(request: NextRequest) {
   const headersList = await headers();
   console.log(headersList.get("Authorization"));
 
+  const theme = request.cookies.get("theme");
+  console.log("Theme cookie:", theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultsPerPage", "10", {});
+  console.log(cookieStore.get("resultsPerPage"));
+
   return new Response("<h1>Profile API Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "set-Cookie": "theme=dark",
     },
   });
 }
